6aad24dc541bd1719e776eef2a3058b2dbd4782d
ISSAM <issamlaafer509@gmail.com>
2024-03-01 12:17:11 +0100

    getting the target_push function smaller

--- target.c
+++ target.c
@@ -6,7 +6,7 @@
 /*   By: lissam <lissam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 09:20:35 by lissam            #+#    #+#             */
-/*   Updated: 2024/03/01 10:50:58 by lissam           ###   ########.fr       */
+/*   Updated: 2024/03/01 12:08:21 by lissam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,19 +96,28 @@ void	push(t_data **stack_a, t_data **stack_b, t_elements *elements)
 	push_to_b(stack_a, stack_b, elements);
 }
 
-void closest_smaller()
+void	closest_smaller(int nb,int *target, t_data **stack_b)
 {
-	
+	t_data	*tmp_b;
+
+	tmp_b = *stack_b;
+	*target = find_first_target(nb, stack_b);
+	while (tmp_b)
+	{
+		if (tmp_b->data < nb && tmp_b->data > *target)
+			*target = tmp_b->data;
+		tmp_b = tmp_b->next;
+	}
 }
 
 void	target_push(t_data **stack_a, t_data **stack_b)
 {
 	t_data		*tmp_a;
-	t_data		*tmp_b;
 	int			target;
 	int			number_moves;
 	t_elements	elements;
 
+	// t_data		*tmp_b;
 	tmp_a = *stack_a;
 	number_moves = 500;
 	while (tmp_a)
@@ -117,14 +126,15 @@ void	target_push(t_data **stack_a, t_data **stack_b)
 			target = the_big_n(stack_b);
 		else
 		{
-			tmp_b = *stack_b;
-			target = find_first_target(tmp_a->data, stack_b);
-			while (tmp_b)
-			{
-				if (tmp_b->data < tmp_a->data && tmp_b->data > target)
-					target = tmp_b->data;
-				tmp_b = tmp_b->next;
-			}
+			// tmp_b = *stack_b;
+			// target = find_first_target(tmp_a->data, stack_b);
+			// while (tmp_b)
+			// {
+			// 	if (tmp_b->data < tmp_a->data && tmp_b->data > target)
+			// 		target = tmp_b->data;
+			// 	tmp_b = tmp_b->next;
+			// }
+			closest_smaller(tmp_a->data,&target,stack_b);
 		}
 		if (count_moves(tmp_a->data, target, stack_a, stack_b) < number_moves)
 		{
